3920b2dc6740f89193eae5ee554afb6c8064ad54
SvZor <zorinasp@yandex.ru>
2024-09-28 22:15:30 +0900

    домашка2

--- HWSem4.py
+++ HWSem4.py
@@ -4,15 +4,12 @@ array_2 = [6, 7, 20, 80, 100]
 array_3 = [3, 4, 15, 20, 30, 70, 80, 120]
 # Задача 1: найти элементы, которые есть в каждом списке
 # Решение без использования множеств
-all_elems = array_1 + array_2 + array_3 # Объединяем все списки в
-один
+all_elems = array_1 + array_2 + array_3 # Объединяем все списки в один
 new_elems_1 = []
 for elem in all_elems:
-# Проверяем, если элемент не добавлен ранее и присутствует во
-всех трех списках
-if elem not in new_elems_1 and all(elem in array for array in
-[array_1, array_2, array_3]):
-new_elems_1.append(elem) # Добавляем элемент в результат
+# Проверяем, если элемент не добавлен ранее и присутствует во всех трех списках
+    if elem not in new_elems_1 and all(elem in array for array in [array_1, array_2, array_3]):
+        new_elems_1.append(elem) # Добавляем элемент в результат
 print("Решение без множеств:", new_elems_1)
 # Решение с использованием множеств
 new_elems_1_set = set(array_1) & set(array_2) & set(array_3) #
